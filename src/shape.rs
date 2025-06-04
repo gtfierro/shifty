0be8e0aa932e76adce5ccde2948d3bbd1a3921bc
Gabe Fierro <gtfierro@mines.edu>
2025-06-04 15:45:02 -0400

    refactor: Use Target and ComponentID in NodeShape

--- src/shape.rs
+++ src/shape.rs
@@ -1,5 +1,5 @@
-use crate::types::ID;
-use crate::types::Path;
+use crate::types::{ID, ComponentID};
+use crate::types::{Path, Target};
 use crate::named_nodes::SHACL;
 use oxigraph::model::{Term, NamedNode, TermRef};
 
@@ -10,9 +10,9 @@ pub enum Shape {
 
 pub struct NodeShape {
     identifier: ID,
-    targets: Vec<ID>,
+    targets: Vec<Target>,
     property_shapes: Vec<ID>,
-    constraints: Vec<ID>,
+    constraints: Vec<ComponentID>,
     // TODO severity
     // TODO message
 }
@@ -23,36 +23,4 @@ pub struct PropertyShape {
     constraints: Vec<ID>,
     // TODO severity
     // TODO message
-}
-
-pub enum Target {
-    Class(Term),
-    Node(Term),
-    SubjectsOf(NamedNode),
-    ObjectsOf(NamedNode),
-}
-
-impl Target {
-    pub fn from_predicate_object(predicate: TermRef, object: TermRef) -> Self {
-        let shacl = SHACL::new();
-        match predicate {
-            shacl.target_class => Target::Class(object.into_owned()),
-            shacl.target_node => Target::Node(object.into_owned()),
-            shacl.target_subjects_of => {
-                if let TermRef::NamedNode(nn) = object {
-                    Target::SubjectsOf(nn.into_owned())
-                } else {
-                    panic!("Expected NamedNode for target_subjects_of")
-                }
-            },
-            shacl.target_objects_of => {
-                if let TermRef::NamedNode(nn) = object {
-                    Target::ObjectsOf(nn.into_owned())
-                } else {
-                    panic!("Expected NamedNode for target_objects_of")
-                }
-            },
-            _ => panic!("Unknown predicate for target: {:?}", predicate),
-        }
-    }
 }
